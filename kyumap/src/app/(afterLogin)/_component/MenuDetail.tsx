3dd062281212c2e9e370c0df0472ae4ce2c2830f
JongKyuHong <kongi7064@gmail.com>
2024-01-15 19:19:27 +0900

    feat: 1차 다크모드 실험

--- kyumap/src/app/(afterLogin)/_component/MenuDetail.tsx
+++ kyumap/src/app/(afterLogin)/_component/MenuDetail.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import styles from "./menudetail.module.css";
 import { useCallback, useState } from "react";
+import DarkMode from "./DarkMode";
 
 export default function MenuDetail() {
   const [clicked, setClicked] = useState(false);
@@ -342,78 +343,7 @@ export default function MenuDetail() {
                                   </div>
                                 </div>
                               </div>
-                              <div className={styles.darkDiv}>
-                                <div
-                                  className={styles.navLink}
-                                  onClick={onClickDark}
-                                >
-                                  <div className={styles.linkDiv}>
-                                    <div className={styles.linkInnerDiv}>
-                                      <div className={styles.linkInnerDiv2}>
-                                        <div className={styles.iconTitle}>
-                                          <div className={styles.titleInner}>
-                                            <div className={styles.spanOuter}>
-                                              <span className={styles.Span}>
-                                                <span
-                                                  className={styles.innerSpan}
-                                                >
-                                                  다크 모드
-                                                </span>
-                                              </span>
-                                            </div>
-                                          </div>
-                                        </div>
-                                        <div className={styles.navIcon}>
-                                          <div className={styles.innerIcon}>
-                                            <div
-                                              className={
-                                                darkMode
-                                                  ? styles.innerIconDiv2Dark
-                                                  : styles.innerIconDiv2
-                                              }
-                                            >
-                                              <div
-                                                className={
-                                                  darkMode
-                                                    ? styles.iconDivDark
-                                                    : styles.iconDiv
-                                                }
-                                              ></div>
-                                              <div
-                                                className={
-                                                  darkMode
-                                                    ? styles.iconDiv2Dark
-                                                    : styles.iconDiv2
-                                                }
-                                              ></div>
-                                              <input
-                                                dir="ltr"
-                                                aria-checked={
-                                                  darkMode ? "true" : "false"
-                                                }
-                                                role="switch"
-                                                className={
-                                                  darkMode
-                                                    ? styles.iconInputDark
-                                                    : styles.iconInput
-                                                }
-                                                type="checkbox"
-                                              ></input>
-                                            </div>
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                  <div
-                                    className={
-                                      darkMode
-                                        ? styles.linkDivOuter2
-                                        : styles.linkDivOuter
-                                    }
-                                  ></div>
-                                </div>
-                              </div>
+                              <DarkMode />
                             </div>
                           </div>
                         </div>
