103b2c656e246e1ec3bfc33beb7d7f53dc089151
JongKyuHong <kongi7064@gmail.com>
2024-01-30 13:48:45 +0900

    이모티콘 모달 수정

--- kyumap/src/app/(afterLogin)/_component/Emoticon.tsx
+++ kyumap/src/app/(afterLogin)/_component/Emoticon.tsx
@@ -3,45 +3,92 @@
 import { useEffect, useState } from "react";
 import styles from "./emoticon.module.css";
 
-export default function Emoticon() {
-  const [viewportSize, setViewportSize] = useState({ width: 0, height: 0 });
+interface propsType {
+  top: number;
+  left: number;
+}
+
+export default function Emoticon({ top, left }: propsType) {
+  // const [viewportSize, setViewportSize] = useState({ width: 0, height: 0 });
+  const [modalPosition, setModalPosition] = useState({ width: 0, height: 0 });
   const [isModalVisible, setIsModalVisible] = useState(true);
-  const debounce = (func: (...args: any[]) => void, delay: number) => {
-    let timerId: NodeJS.Timeout;
+  // const debounce = (func: (...args: any[]) => void, delay: number) => {
+  //   let timerId: NodeJS.Timeout;
 
-    return function (...args: any[]) {
-      if (timerId) {
-        clearTimeout(timerId);
-      }
+  //   return function (...args: any[]) {
+  //     if (timerId) {
+  //       clearTimeout(timerId);
+  //     }
 
-      timerId = setTimeout(() => {
-        func(...args);
-        timerId = null!;
-      }, delay);
-    };
-  };
+  //     timerId = setTimeout(() => {
+  //       func(...args);
+  //       timerId = null!;
+  //     }, delay);
+  //   };
+  // };
 
-  const handleResize = debounce(() => {
-    setViewportSize({
-      width: window.innerWidth || document.documentElement.clientWidth,
-      height: window.innerHeight || document.documentElement.clientHeight,
-    });
-  }, 3); // 300ms 딜레이
+  // const handleResize = debounce(() => {
+  //   // setViewportSize({
+  //   //   width: window.innerWidth || document.documentElement.clientWidth,
+  //   //   height: window.innerHeight || document.documentElement.clientHeight,
+  //   // });
+
+  //   setModalPosition({
+  //     // width: window.innerWidth || document.documentElement.clientWidth,
+  //     // height: window.innerHeight || document.documentElement.clientHeight,
+  //     width: left,
+  //     height: top,
+  //   });
+  // }, 3); // 300ms 딜레이
+
+  // useEffect(() => {
+  //   // 화면 크기가 변경될 때마다 실행되는 이벤트 핸들러 등록
+  //   window.addEventListener("resize", handleResize);
+  //   handleResize();
+  //   // 컴포넌트가 언마운트될 때 이벤트 핸들러 제거
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
+
+  // const translateValue = `${viewportSize.width - 623}px, ${
+  //   viewportSize.height - 69
+  // }px`;
+
+  // return (
+  //   <div
+  //     className={styles.rootDiv}
+  //     style={{
+  //       transform: `translate(${translateValue}) translate(0px, -100%)`,
+  //       display: isModalVisible ? "block" : "none",
+  //     }}
+  //   >
+  // useEffect(() => {
+  //   setModalPosition({
+  //     width: left,
+  //     height: top,
+  //   });
+  // }, [left, top]);
+
+  // const translateValue = `${modalPosition.width}px, ${modalPosition.height}px`;
+
+  // return (
+  //   <div
+  //     className={styles.rootDiv}
+  //     style={{
+  //       transform: `translate(${translateValue}) translate(0px, -100%)`,
+  //       display: isModalVisible ? "block" : "none",
+  //     }}
+  //   >
 
   useEffect(() => {
-    // 화면 크기가 변경될 때마다 실행되는 이벤트 핸들러 등록
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    // 컴포넌트가 언마운트될 때 이벤트 핸들러 제거
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    setModalPosition({
+      width: left,
+      height: top,
+    });
   }, []);
 
-  const translateValue = `${viewportSize.width - 623}px, ${
-    viewportSize.height - 69
-  }px`;
-
+  const translateValue = `${modalPosition.width}px, ${modalPosition.height}px`;
   return (
     <div
       className={styles.rootDiv}
