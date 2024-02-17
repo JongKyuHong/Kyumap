303d22848625c58a8b0787ca737d5aefb1952843
JongKyu <kongi7064@gmail.com>
2024-02-17 20:35:21 +0900

    전환 로그인 모달

---
+++ kyumap/src/app/(afterLogin)/NewLogin/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "../home/page";
+import LoginModal from "../_component/LoginModal";
+
+export default function page() {
+  return (
+    <>
+      <Home />
+      <LoginModal />
+    </>
+  );
+}
