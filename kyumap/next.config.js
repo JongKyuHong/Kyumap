0672c2964c9b21cd05a800d100e942eab58ac703
JongKyu <kongi7064@gmail.com>
2024-06-23 12:00:26 +0900

    config 수정

--- kyumap/next.config.js
+++ kyumap/next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/upload/:slug",
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/upload/:slug`,
-      },
-    ];
-  },
   reactStrictMode: false,
   images: {
     remotePatterns: [
