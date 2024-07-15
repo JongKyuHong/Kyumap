4e7a63279d601cd15a0190c3283f4a02553e7e88
JongKyu <kongi7064@gmail.com>
2024-07-15 17:07:23 +0900

    redis제거

--- kyumap/src/app/(afterLogin)/layout.tsx
+++ kyumap/src/app/(afterLogin)/layout.tsx
@@ -1,15 +1,11 @@
 import React, { ReactNode } from "react";
 import styles from "./layout.module.css";
 import RQProvider from "./_component/RQProvider";
-import { auth } from "@/auth";
-import NavTab from "./_component/NavTab";
-import FollowRecommendSection from "./_component/FollowRecommendSection";
 import Nav from "./_component/Nav";
 
 type Props = { children: ReactNode; modal: ReactNode };
 
 export default async function RootLayout({ children, modal }: Props) {
-  const session = await auth();
   return (
     <div>
       <RQProvider>
@@ -24,23 +20,18 @@ export default async function RootLayout({ children, modal }: Props) {
               <div className={styles.leafParent}>
                 <div className={styles.leafChild}>
                   <div className={styles.container} style={{ height: "100%" }}>
-                    {session && session.user && (
-                      <>
-                        {/* <NavTab me={session} /> */}
-                        <Nav />
-                        <div className={styles.rightSectionWrapper}>
-                          <section className={styles.rootSection}>
-                            <main
-                              className={styles.rootMain}
-                              style={{ height: "90vh", overflowY: "auto" }}
-                              role="main"
-                            > 
-                              {children}
-                            </main>
-                          </section>
-                        </div>
-                      </>
-                    )}
+                    <Nav />
+                    <div className={styles.rightSectionWrapper}>
+                      <section className={styles.rootSection}>
+                        <main
+                          className={styles.rootMain}
+                          style={{ height: "90vh", overflowY: "auto" }}
+                          role="main"
+                        >
+                          {children}
+                        </main>
+                      </section>
+                    </div>
                   </div>
                 </div>
               </div>
