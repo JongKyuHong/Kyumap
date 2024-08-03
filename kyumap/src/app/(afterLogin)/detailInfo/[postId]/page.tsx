3a10ba92a01bc7a9aad0b4a3f90a25d4e64dba2e
JongKyu <kongi7064@gmail.com>
2024-08-03 15:08:26 +0900

    reel페이지 detailInfo페이지 통합 및 home모달 수정

---
+++ kyumap/src/app/(afterLogin)/detailInfo/[postId]/page.tsx
@@ -0,0 +1,30 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import ReelPost from "./_component/ReelPost";
+import { getComments } from "../../_lib/getComments";
+import { getPost } from "../../_lib/getPost";
+
+type Props = {
+  params: {
+    postId: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", params.postId.toString()],
+    queryFn: getPost,
+  });
+
+  const dehydrateState = dehydrate(queryClient);
+  return (
+    <HydrationBoundary state={dehydrateState}>
+      <ReelPost postId={params.postId} />;
+    </HydrationBoundary>
+  );
+}
